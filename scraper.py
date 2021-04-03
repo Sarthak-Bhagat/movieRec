735f241c1f92e4657dc49fcb8f7f3e9ea67ca8f8
Sarthak-Bhagat <sarth.bhagat@gmail.com>
2021-04-03 19:59:27 +0530

    Better scrape ID

--- scraper.py
+++ scraper.py
@@ -1,26 +1,23 @@
-import json
+import concurrent.futures
 import csv
-import requests
+import json
 import time
+
+import numpy as np
+import requests
 import tqdm
 from bs4 import BeautifulSoup, SoupStrainer
-import concurrent.futures
 
 
-def scrape(URL):
+def scrape_ids_from_imdb(URL):
     page = requests.get(URL)
     strainer = SoupStrainer('a', href=True)
     soup = BeautifulSoup(page.content, "lxml", parse_only=strainer)
-    return(soup)
-
-
-def get_links_from_soup(soup):
-    L = []
+    list_of_ids = []
     for a in soup.find_all("a", href=lambda href: href.startswith("/title")):
-        L.append(a['href'])
-    L = list(dict.fromkeys(L))
-    for i in L:
-        print(i[7:-1])
+        list_of_ids.append(a['href'][7:-1])
+    list_of_ids = list(dict.fromkeys(list_of_ids))
+    return(list_of_ids)
 
 
 def main():
@@ -28,17 +25,16 @@ def main():
     no_of_pages = entries_to_scrape//50
     num = 0
     baseURL = "http://www.imdb.com/search/title"
-    scraped_pages = []
+    L = []
     start = time.perf_counter()
 
     for i in tqdm.trange(no_of_pages):
         num = i*50
         addr = f"/?year=2005-01-01,2021-12-31&view=simple&start={num}"
-        scraped_pages.append(scrape(baseURL+addr))
+        L.append(scrape_ids_from_imdb(baseURL+addr))
 
-    for i in scraped_pages:
-        get_links_from_soup(i)
-
+    imdb_ids = np.array(L).flatten()
+    print(imdb_ids)
     finish = time.perf_counter()
     print(finish-start)
 
